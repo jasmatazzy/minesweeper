b1164c9d00b0ad816890574f0d2c8370c62394d8
Jasmine Hendricks <jhendricks@mparticle.com>
2024-07-18 17:19:55 -0600

    user settings as form

--- src/components/UserSettings/index.tsx
+++ src/components/UserSettings/index.tsx
@@ -1,21 +1,49 @@
 import React from "react";
+import startGameIcon from "../../assets/icon-start-game.png";
 
 interface UserSettingsProps {
-  handleClick: () => void;
+  getMineCount: () => void;
   numberOfMinesOnBoard: number;
+  startGame: () => void;
 }
 
-const UserSettings = (props
-: UserSettingsProps
-): JSX.Element => {
-  const {
-    handleClick,
-    numberOfMinesOnBoard
-  } = props;
+const UserSettings = (props: UserSettingsProps): JSX.Element => {
+  const { getMineCount, numberOfMinesOnBoard, startGame } = props;
   return (
-    <div>
-      <input type="number" id="mine-count" value={numberOfMinesOnBoard} onChange={handleClick}></input>
-    </div>
+    <form
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        maxWidth: numberOfMinesOnBoard * 15,
+        // This should be number of columns * some constant so that it's always relative
+      }}
+    >
+      <div>
+        <label htmlFor="mine-count">How many mines?:</label>
+        <input
+          id="mine-count"
+          onChange={getMineCount}
+          type="number"
+          value={numberOfMinesOnBoard}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="start-game">Press Start ➡:</label>
+        <input
+          id="start-game"
+          alt="Submit"
+          height="48"
+          name="start-game"
+          src={startGameIcon}
+          type="image"
+          width="48"
+          onClick={(event)=>{
+            event.preventDefault();
+            startGame();
+          }}
+        ></input>
+      </div>
+    </form>
   );
 };
 
