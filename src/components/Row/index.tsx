c1e1cf33f3b79df141ccb292e4da04252cb6c9ad
Jasmine Hendricks <jhendricks@mparticle.com>
2024-07-25 15:42:37 -0600

    minor style update

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -2,7 +2,10 @@ import React, { PropsWithChildren } from "react";
 
 const Row = ({children}: PropsWithChildren) => {
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex",
+    margin: 0,
+    padding: 0,
+     }}>
       {children}
     </div>
   );
