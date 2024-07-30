52f63626dad2eb706a302998eab63e2d451fc1d5
Jasmine Hendricks <jhendricks@mparticle.com>
2024-07-29 22:12:05 -0600

    Bojack joining the temp team

--- src/components/Gameboard/index.tsx
+++ src/components/Gameboard/index.tsx
@@ -16,12 +16,10 @@ interface GameboardProps {
   };
   handleMainClick: (row: number, column: number) => void;
   handleSquareDoubleClick: (row: number, column: number) => void;
-  handleSquareRightClick: (
-    row: number,
-    column: number
-  ) => void;
+  handleSquareRightClick: (row: number, column: number) => void;
   isGameOver: boolean;
   isGameStarted: boolean;
+  isGameWon: boolean;
   numberOfMinesOnBoard: number;
   numberOfNeighborsWhoAreFlags: (row: number, column: number) => number;
   numberOfNeighborsWhoAreMines: {
@@ -41,6 +39,7 @@ const Gameboard = (props: GameboardProps): JSX.Element => {
     handleSquareRightClick,
     isGameOver,
     isGameStarted,
+    isGameWon,
     numberOfNeighborsWhoAreFlags,
     numberOfNeighborsWhoAreMines,
     numberOfRowsOnBoard,
@@ -59,54 +58,62 @@ or columns, or when isGameStarted is set to true; Live board displays on final u
 
   useEffect(() => {
     if (isGameOver) {
-      console.log(`game over`);
     }
+    
   }, [isGameOver]);
 
   return (
     <div>
-      {Array.from({ length: numberOfRowsOnBoard }, (_, rowIndex) => (
-        <Row key={rowIndex}>
-          {Array.from(
-            { length: numberOfSquaresOnEachRow },
-            (_, columnIndex) => (
-              <SquareButton
-                key={columnIndex}
-                handleMainClick={() => handleMainClick(rowIndex, columnIndex)}
-                handleSquareDoubleClick={() =>
-                  handleSquareDoubleClick(rowIndex, columnIndex)
-                }
-                handleSquareRightClick={(
-                ) => handleSquareRightClick( rowIndex, columnIndex)}
-                isGameStarted={isGameStarted}
-                isGameOver={isGameOver}
-                isMine={
-                  (gameboardMineSquareLocations[rowIndex] &&
-                    gameboardMineSquareLocations[rowIndex][columnIndex]) ??
-                  false
-                }
-                isSquareOpen={
-                  (gameboardOpenSquareLocations[rowIndex] &&
-                    gameboardOpenSquareLocations[rowIndex][columnIndex]) ??
-                  false
-                }
-                isFlagged={
-                  (gameboardFlagSquareLocations[rowIndex] &&
-                    gameboardFlagSquareLocations[rowIndex][columnIndex]) ??
-                  false
-                }
-                numberOfNeighborsWhoAreMines={
-                  numberOfNeighborsWhoAreMines[rowIndex] &&
-                  numberOfNeighborsWhoAreMines[rowIndex][columnIndex]
-                    ? numberOfNeighborsWhoAreMines[rowIndex][columnIndex]
-                    : 0
-                }
-                // optionalText={JSON.stringify(`${numberOfNeighborsWhoAreFlags(rowIndex, columnIndex)}`)}
-              />
-            )
-          )}
-        </Row>
-      ))}
+      {isGameWon ? (
+        <img
+          src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGh4aHIwMDk4OGlhbmNpbzU0MDJicjFwb2V6dnI3bzl4MmxjcjNlciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT0GqH01ZyKwd3aT3G/giphy.gif"
+          alt="bojack-is-happy-for-you"
+        />
+      ) : (
+        Array.from({ length: numberOfRowsOnBoard }, (_, rowIndex) => (
+          <Row key={rowIndex}>
+            {Array.from(
+              { length: numberOfSquaresOnEachRow },
+              (_, columnIndex) => (
+                <SquareButton
+                  key={columnIndex}
+                  handleMainClick={() => handleMainClick(rowIndex, columnIndex)}
+                  handleSquareDoubleClick={() =>
+                    handleSquareDoubleClick(rowIndex, columnIndex)
+                  }
+                  handleSquareRightClick={() =>
+                    handleSquareRightClick(rowIndex, columnIndex)
+                  }
+                  isGameStarted={isGameStarted}
+                  isGameOver={isGameOver}
+                  isMine={
+                    (gameboardMineSquareLocations[rowIndex] &&
+                      gameboardMineSquareLocations[rowIndex][columnIndex]) ??
+                    false
+                  }
+                  isSquareOpen={
+                    (gameboardOpenSquareLocations[rowIndex] &&
+                      gameboardOpenSquareLocations[rowIndex][columnIndex]) ??
+                    false
+                  }
+                  isFlagged={
+                    (gameboardFlagSquareLocations[rowIndex] &&
+                      gameboardFlagSquareLocations[rowIndex][columnIndex]) ??
+                    false
+                  }
+                  numberOfNeighborsWhoAreMines={
+                    numberOfNeighborsWhoAreMines[rowIndex] &&
+                    numberOfNeighborsWhoAreMines[rowIndex][columnIndex]
+                      ? numberOfNeighborsWhoAreMines[rowIndex][columnIndex]
+                      : 0
+                  }
+                  // optionalText={JSON.stringify(`${numberOfNeighborsWhoAreFlags(rowIndex, columnIndex)}`)}
+                />
+              )
+            )}
+          </Row>
+        ))
+      )}
     </div>
   );
 };
